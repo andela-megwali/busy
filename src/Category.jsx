3e679aa62cff8fae46cc5d354164d25d3daf84c6
Michael <michael.egwali@andela.com>
2017-10-31 12:12:34 +0100

    Add todoitmem skeletal component

--- src/Category.jsx
+++ src/Category.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoItem from './TodoItem';
 
 class Category extends React.Component {
   constructor(props) {
@@ -11,7 +12,14 @@ class Category extends React.Component {
     return (
       <div>
         <input />
-        <button type={'button'} className={'btn btn-primary'}>{'Add Category'}</button>
+        <button
+          type={'button'}
+          className={'btn btn-primary'}
+        >
+          {'Add Category'}
+        </button>
+
+        <TodoItem />
       </div>
     );
   }
---
+++ src/TodoItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        TodoItem
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
